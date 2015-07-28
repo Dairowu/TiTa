c5dadc3340cd36267887d0af18271883a8d77773
Dairowu <Dairowu@163.com>
2015-07-28 17:59:54 +0800

    Added

--- app/src/main/java/xietong/tita/TiTa.java
+++ app/src/main/java/xietong/tita/TiTa.java
@@ -1,17 +1,23 @@
 package xietong.tita;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TiTa extends ActionBarActivity {
+public class TiTa extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ti_ta);
+
+        getSupportActionBar().setTitle(null);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
     }
 
     @Override
