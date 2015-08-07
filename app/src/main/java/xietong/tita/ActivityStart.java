13928751b312e5293c5aff8a70978590d8f8fba7
acer-PC <540740324@qq.com>
2015-08-07 11:02:37 +0800

    换肤修改

--- app/src/main/java/xietong/tita/ActivityStart.java
+++ app/src/main/java/xietong/tita/ActivityStart.java
@@ -1,13 +1,16 @@
 package xietong.tita;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 
 /**
  * Created by acer-PC on 2015/8/6.
  * 登录时只显示一张图片
- *
  */
 public class ActivityStart extends Activity {
     @Override
@@ -16,6 +19,20 @@ public class ActivityStart extends Activity {
         setContentView(R.layout.activity_start);
         FinishApp.addActivity(this);
 
+        //如果换肤的结果是为空的话
+        String string = getSharedPreferences("music_play", MODE_WORLD_READABLE)
+                .getString("background", null);
+        SharedPreferences.Editor editor = getSharedPreferences("music_play", MODE_WORLD_READABLE).edit();
+        if (string.equals(null)) {
+            Resources r = getResources();
+            Uri uri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://"
+                    + r.getResourcePackageName(R.drawable.bg) + "/"
+                    + r.getResourceTypeName(R.drawable.bg) + "/"
+                    + r.getResourceEntryName(R.drawable.bg));
+            editor.putString("background", uri.toString());
+            editor.commit();
+        }
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -23,9 +40,10 @@ public class ActivityStart extends Activity {
                 try {
                     //加载音乐到list
                     Utils.loadFromSD(ActivityStart.this);
-                    Intent intent = new Intent(ActivityStart.this,MainActivity.class);
+                    Intent intent = new Intent(ActivityStart.this, MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
-                    Thread.sleep(1300);
+                    startService(new Intent(ActivityStart.this, LockService.class));
+                    Thread.sleep(1400);
                     startActivity(intent);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
